0033900b6ca2a06ba9d8893f6fab447806495f71
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-06-17 13:29:29 -0300

    refactor: define an asynchronous function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,25 @@
-let result = document.getElementById("result")
+let result = document.getElementById("result");
 
-let URL = "https://api.adviceslip.com/advice"
+let URL = "https://api.adviceslip.com/advice";
 
-const generator = () => {
-    fetch(URL)
+const generator = async () => {
+    const dados = await fetch(URL)
         .then(
             response => response.json()
         )
         .then(data => {
             console.log(data.slip)
-
             result.innerHTML = `
                 <p class="textGenerator">${data.slip.advice}</p>
+                <p class="textGenerator">${data.slip.id }</p>
             `
-
         })
+        .catch(() => {
+            result.innerHTML = `
+                <p class="textGenerator">erro</p>
+            `
+        });
 
-}
+};
 
-generator()
+generator();
